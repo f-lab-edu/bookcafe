199846023be9342b99cc0618d8f191f111433b00
dohun <shine_17@naver.com>
2025-01-12 22:32:39 +0900

    null 처리 로직을 Optional 사용하여 변경

--- src/main/java/com/study/bookcafe/infrastructure/command/borrow/TestBorrowRepository.java
+++ src/main/java/com/study/bookcafe/infrastructure/command/borrow/TestBorrowRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class TestBorrowRepository implements BorrowRepository {
@@ -42,11 +43,13 @@ public class TestBorrowRepository implements BorrowRepository {
     public void cancelReservation(long reservationId) {
         long memberId = 1L;
 
-        MembersReservationDetails membersReservationDetails = TestBorrowQueryStorage.membersReservations.get(memberId).stream()
+        Optional<MembersReservationDetails> membersReservationDetails =  TestBorrowQueryStorage.membersReservations.get(memberId).stream()
                 .filter(reservation -> reservation.getId() == reservationId)
-                .findFirst()
-                .orElse(null);
+                .findFirst();
 
-        TestBorrowQueryStorage.membersReservations.get(memberId).remove(membersReservationDetails);
+        membersReservationDetails.ifPresent(
+                TestBorrowQueryStorage.membersReservations.get(memberId)::remove
+        );
+
     }
 }

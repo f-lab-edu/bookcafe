0c0ee5eafdc6bbcda6d54b762be9ca2a417a3203
dohun <shine_17@naver.com>
2025-01-26 14:41:24 +0900

    기간과 대출 기간의 도메인 분리 - BorrowPeriod (대출 기간) 생성 - Period (기간) 범용적인 도메인으로 변경

---
+++ src/main/java/com/study/bookcafe/domain/borrow/BorrowPeriod.java
@@ -0,0 +1,46 @@
+package com.study.bookcafe.domain.borrow;
+
+import com.study.bookcafe.domain.member.Level;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import java.time.LocalDate;
+
+@EqualsAndHashCode
+public class BorrowPeriod {
+
+    private final Period period;
+
+    public static BorrowPeriod of(@NonNull LocalDate from, Level level) {
+        return new BorrowPeriod(from, level);
+    }
+
+    private BorrowPeriod(@NonNull LocalDate from, Level level) {
+        try {
+            this.period = new Period(from, from.plusWeeks(level.getBorrowPeriod()));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("반납 일자는 대출 일자보다 이후여야 합니다.");
+        }
+    }
+
+    private BorrowPeriod(Period period) {
+        this.period = period;
+    }
+
+    public BorrowPeriod(LocalDate from, LocalDate to) {
+        try {
+            this.period = new Period(from, to);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("반납 일자는 대출 일자보다 이후여야 합니다.");
+        }
+    }
+
+    public BorrowPeriod extend(Level level) {
+        return new BorrowPeriod(period.extendByWeeks(level.getExtendPeriod()));
+    }
+
+    public boolean isExtendable(LocalDate now) {
+        LocalDate targetDate = period.getMidDate().minusDays(1);
+        return now.isAfter(targetDate);
+    }
+
+}

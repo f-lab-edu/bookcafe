1347284bb9838b0f06f01c9f717cac5c37125e3e
dohun <shine_17@naver.com>
2025-03-11 21:18:16 +0900

    오타 수정

--- src/main/java/com/study/bookcafe/application/command/book/BookInventoryService.java
+++ src/main/java/com/study/bookcafe/application/command/book/BookInventoryService.java
@@ -5,7 +5,7 @@ import com.study.bookcafe.domain.book.BookInventory;
 public interface BookInventoryService {
 
     // 도서 조회 (id)
-    BookInventory findById(long bookId);
+    BookInventory findById(long id);
     BookInventory findByBookId(long bookId);
 
 }

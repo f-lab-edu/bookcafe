7c3045eb53469ec91d18c031a566879df79d07d4
dohun <shine_17@naver.com>
2025-03-18 15:33:19 +0900

    - 예약, 반납 책임을 회원으로부터 분리 - 회원 조회 시 반환 자료형 Optional로 변경

--- src/main/java/com/study/bookcafe/application/command/member/MemberService.java
+++ src/main/java/com/study/bookcafe/application/command/member/MemberService.java
@@ -7,12 +7,6 @@ public interface MemberService {
     // 회원 조회 (id)
     Member findById(long memberId);
 
-    // 도서 예약
-    void reserveBook(long memberId, long bookId);
-
-    // 도서 예약 취소
-    void cancelReservation(long reservationId);
-
     // 도서 반납
-    void returnBook(long memberId, long bookId);
+//    void returnBook(long memberId, long bookId);
 }
--- src/main/java/com/study/bookcafe/application/query/member/MemberQueryService.java
+++ src/main/java/com/study/bookcafe/application/query/member/MemberQueryService.java
@@ -1,19 +1,8 @@
 package com.study.bookcafe.application.query.member;
 
-import com.study.bookcafe.query.borrow.BorrowDetails;
-import com.study.bookcafe.domain.member.Member;
-import com.study.bookcafe.query.member.MembersReservationDetails;
-
-import java.util.List;
+import com.study.bookcafe.query.member.MemberView;
 
 public interface MemberQueryService {
-
     // 회원 조회 (id)
-    Member findById(long memberId);
-
-    // 도서 대출 조회
-    List<BorrowDetails> findBorrows(long memberId);
-
-    // 도서 예약 조회
-    List<MembersReservationDetails> findMembersReservationDetails(long memberId);
+    MemberView findById(long memberId);
 }

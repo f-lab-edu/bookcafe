1b758199b1b3861f572a73be04aaec6b0f50e65f
dohun <shine_17@naver.com>
2025-04-30 14:29:23 +0900

    Refactor: 우선대출권 삭제에 따른 코드수정

--- src/main/java/com/study/bookcafe/infrastructure/query/member/MemberTestSets.java
+++ src/main/java/com/study/bookcafe/infrastructure/query/member/MemberTestSets.java
@@ -2,7 +2,6 @@ package com.study.bookcafe.infrastructure.query.member;
 
 import com.study.bookcafe.domain.member.Level;
 import com.study.bookcafe.domain.member.Member;
-import com.study.bookcafe.infrastructure.query.borrow.PriorityBorrowRightTestSets;
 import com.study.bookcafe.interfaces.member.MemberDto;
 import com.study.bookcafe.query.member.MemberView;
 
@@ -28,7 +27,7 @@ public class MemberTestSets {
         return Member.builder().id(1L).name("슈카").level(Level.BASIC).borrowCount(0).reservationCount(1).build();
     }
     public static Member createWormMember() {
-        return Member.builder().id(2L).name("머스크").level(Level.WORM).borrowCount(3).reservationCount(2).priorityBorrowRightsMap(PriorityBorrowRightTestSets.createWormPriorityBorrowRightMap).build();
+        return Member.builder().id(2L).name("머스크").level(Level.WORM).borrowCount(3).reservationCount(2).build();
     }
     public static Member createLibrarianMember() {
         return Member.builder().id(3L).name("트럼프").level(Level.LIBRARIAN).borrowCount(5).build();

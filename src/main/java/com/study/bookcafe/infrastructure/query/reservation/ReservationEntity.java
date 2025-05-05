99610abe8af42cc7877aaf9779ec8d2ac9f4af8a
dohun <shine_17@naver.com>
2025-05-05 15:28:36 +0900

    Refactor: VO와 Entity 매핑

--- src/main/java/com/study/bookcafe/infrastructure/query/reservation/ReservationEntity.java
+++ src/main/java/com/study/bookcafe/infrastructure/query/reservation/ReservationEntity.java
@@ -3,10 +3,7 @@ package com.study.bookcafe.infrastructure.query.reservation;
 import com.study.bookcafe.domain.borrow.PriorityBorrowPeriod;
 import com.study.bookcafe.infrastructure.query.book.BookInventoryEntity;
 import com.study.bookcafe.infrastructure.query.member.MemberEntity;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,9 +19,9 @@ import java.time.LocalDateTime;
 public class ReservationEntity {
     @Id
     private long id;                                        // 예약 번호
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private MemberEntity member;                            // 회원
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private BookInventoryEntity book;                       // 도서
     private LocalDateTime time;                             // 예약 시간
     @Embedded

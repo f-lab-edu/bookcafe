bbb9a9ee30f20336082521c2bb4dfd5156759d9d
dohun <shine_17@naver.com>
2025-05-11 16:33:08 +0900

    refactor: 도메인과 엔티티 mapper 수정

--- src/main/java/com/study/bookcafe/interfaces/reservation/ReservationMapper.java
+++ src/main/java/com/study/bookcafe/interfaces/reservation/ReservationMapper.java
@@ -3,6 +3,7 @@ package com.study.bookcafe.interfaces.reservation;
 import com.study.bookcafe.domain.reservation.Reservation;
 import com.study.bookcafe.infrastructure.query.reservation.ReservationEntity;
 import com.study.bookcafe.interfaces.book.BookMapper;
+import com.study.bookcafe.interfaces.borrow.BorrowMapper;
 import com.study.bookcafe.interfaces.member.MemberMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,24 +14,24 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        uses = {MemberMapper.class, BookMapper.class}
+        uses = {MemberMapper.class, BookMapper.class, BorrowMapper.class}
 )
 public interface ReservationMapper {
+    // ReservationEntity -> Reservation
     @Named("ReservationEntityToReservation")
     @Mapping(target = "id", source = "id")
     @Mapping(target = "member", source = "member", qualifiedByName = {"MemberMapper", "MemberEntityToMember"})
     @Mapping(target = "book", source = "book", qualifiedByName = {"BookMapper", "BookInventoryEntityToBookInventory"})
     @Mapping(target = "time", source = "time")
-    @Mapping(target = "priorityBorrowPeriod", source = "priorityBorrowPeriod")
-    // ReservationEntity -> Reservation
+    @Mapping(target = "priorityBorrowPeriod", source = "priorityBorrowPeriod", qualifiedByName = {"BorrowMapper", "PriorityBorrowPeriodEntityToPriorityBorrowPeriod"})
     Reservation toReservation(ReservationEntity reservationEntity);
 
+    // Reservation -> ReservationEntity
     @Named("ReservationToReservationEntity")
     @Mapping(target = "id", source = "id")
     @Mapping(target = "member", source = "member", qualifiedByName = {"MemberMapper", "MemberToMemberEntity"})
     @Mapping(target = "book", source = "book", qualifiedByName = {"BookMapper", "BookInventoryToBookInventoryEntity"})
     @Mapping(target = "time", source = "time")
-    @Mapping(target = "priorityBorrowPeriod", source = "priorityBorrowPeriod")
-    // Reservation -> ReservationEntity
+    @Mapping(target = "priorityBorrowPeriod", source = "priorityBorrowPeriod", qualifiedByName = {"BorrowMapper", "PriorityBorrowPeriodToPriorityBorrowPeriodEntity"})
     ReservationEntity toReservationEntity(Reservation reservation);
 }

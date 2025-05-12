7fd063d09f56ef68b0b7ffd18ec5e961f7297c3d
dohun <shine_17@naver.com>
2025-05-12 16:16:35 +0900

    add: 우선예약 선정 JPA 구현

--- src/main/java/com/study/bookcafe/infrastructure/command/reservation/JpaReservationRepository.java
+++ src/main/java/com/study/bookcafe/infrastructure/command/reservation/JpaReservationRepository.java
@@ -3,9 +3,13 @@ package com.study.bookcafe.infrastructure.command.reservation;
 import com.study.bookcafe.domain.borrow.PriorityBorrowPeriod;
 import com.study.bookcafe.domain.reservation.Reservation;
 import com.study.bookcafe.domain.reservation.ReservationRepository;
+import com.study.bookcafe.infrastructure.query.reservation.ReservationEntity;
 import com.study.bookcafe.interfaces.reservation.ReservationMapper;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,6 +18,9 @@ import java.util.Optional;
 public class JpaReservationRepository implements ReservationRepository {
 
     private final ReservationMapper reservationMapper;
+
+    @PersistenceContext
+    private EntityManager em;
 
     public JpaReservationRepository(ReservationMapper reservationMapper) {
         this.reservationMapper = reservationMapper;
@@ -27,6 +34,21 @@ public class JpaReservationRepository implements ReservationRepository {
     @Override
     public Optional<Reservation> findByMemberIdAndBookId(long memberId, long bookId) {
         return Optional.empty();
+    }
+
+    @Override
+    public Optional<Reservation> findPriorityByMemberIdAndBookId(long memberId, long bookId) {
+        return em
+                .createQuery(
+                        "SELECT r FROM reservation r " +
+                        "WHERE r.member.id = :memberId " +
+                        "AND r.book.bookId = :bookId " +
+                        "AND r.priorityBorrowPeriod IS NOT NULL", ReservationEntity.class)
+                .setParameter("memberId", memberId)
+                .setParameter("bookId", bookId)
+                .getResultStream()
+                .findAny()
+                .map(reservationMapper::toReservation);
     }
 
     @Override
@@ -45,7 +67,26 @@ public class JpaReservationRepository implements ReservationRepository {
     }
 
     @Override
+    @Transactional
     public void selectPriorityReservation(long bookId, PriorityBorrowPeriod period) {
+        em
+                .createQuery(
+                        "SELECT r " +
+                            "FROM reservation r " +
+                            "WHERE r.book.bookId = :bookId " +
+                            "AND r.priorityBorrowPeriod IS NULL " +
+                            "ORDER BY r.id", ReservationEntity.class)
+                .setParameter("bookId", bookId)
+                .setFirstResult(0)
+                .setMaxResults(1)
+                .getResultStream()
+                .findAny()
+                .ifPresent(reservationEntity -> {
+                    Reservation reservation = reservationMapper.toReservation(reservationEntity);
+                    reservation.setPriorityBorrowPeriod(period);
+
+                    em.merge(reservationMapper.toReservationEntity(reservation));
+                });
 
     }
 }
--- src/main/java/com/study/bookcafe/infrastructure/query/reservation/ReservationEntity.java
+++ src/main/java/com/study/bookcafe/infrastructure/query/reservation/ReservationEntity.java
@@ -1,7 +1,7 @@
 package com.study.bookcafe.infrastructure.query.reservation;
 
-import com.study.bookcafe.domain.borrow.PriorityBorrowPeriod;
 import com.study.bookcafe.infrastructure.query.book.BookInventoryEntity;
+import com.study.bookcafe.infrastructure.query.borrow.PriorityBorrowPeriodEntity;
 import com.study.bookcafe.infrastructure.query.member.MemberEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -18,12 +18,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ReservationEntity {
     @Id
-    private long id;                                        // 예약 번호
-    @ManyToOne(fetch = FetchType.LAZY)
-    private MemberEntity member;                            // 회원
-    @ManyToOne(fetch = FetchType.LAZY)
-    private BookInventoryEntity book;                       // 도서
-    private LocalDateTime time;                             // 예약 시간
+    private long id;                                            // 예약 번호
+    @ManyToOne // (fetch = FetchType.LAZY)
+    private MemberEntity member;                                // 회원
+    @ManyToOne // (fetch = FetchType.LAZY)
+    private BookInventoryEntity book;                           // 도서
+    private LocalDateTime time;                                 // 예약 시간
     @Embedded
-    private PriorityBorrowPeriod priorityBorrowPeriod;      // 우선대출기간
+    private PriorityBorrowPeriodEntity priorityBorrowPeriod;    // 우선대출기간
 }

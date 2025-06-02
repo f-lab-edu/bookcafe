987442f367882e831682c02ccb403ddec5baa0dc
dohun <shine_17@naver.com>
2025-06-02 17:00:27 +0900

    chore: JPA 설정 변경 및 더미데이터 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,11 +4,17 @@ VALUES
     (1, 1, 9788936433598, '채식주의자', '한강', NULL, '창비', '2007-10-30', 35000, 5, 1, 0),
     (2, 2, 9788954651134, '흰', '한강', NULL, '문학동네', '2018-04-25', 13000, 2, 2, 1);
 
--- member
-INSERT INTO member(id, password, name, level, borrow_count, reservation_count, create_date, update_date)
+-- borrower
+INSERT INTO borrower(id, password, name, level, borrow_count, reservation_count, create_date, update_date)
 VALUES
     (1, NULL, '슈카', 'BASIC', 0, 1, '2025-05-03 12:00:00', '2025-05-03 12:00:00'),
     (2, NULL, '머스크', 'WORM', 3, 2, '2025-05-03 12:00:00', '2025-05-03 12:00:00');
+
+-- borrow
+INSERT INTO borrow(id, member_id, book_id, time, return_time, "from", "to", extension_count)
+VALUES
+    (1, 1, 1, '2025-06-01 12:00:00', NULL, '2025-06-01', '2025-06-08', 0),
+    (2, 1, 2, '2025-06-01 12:00:00', NULL, '2025-06-01', '2025-06-08', 0);
 
 -- reservation
 INSERT INTO reservation(id, member_id, book_id, time, "from", "to")
